207d923a57f859d222b6e05a4944dd489fcbde09
fjvanest <105886994+fjvanest@users.noreply.github.com>
2022-06-05 22:04:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ __human_name__ = 'strings'
 
 # Add your code after this line
 #Create a variable for every player that scored, for example:
-Goal_scorer_0 = "Ruud Gullit"
-Goal_scorer_1 = "Marco van Basten"
+from gettext import find
+
+
+goal_scorer_0 = "Ruud Gullit"
+goal_scorer_1 = "Marco van Basten"
 
 #Create a variable for each minute of the match that a goal was scored in, for example:
 goal_0 = 32
@@ -15,8 +18,9 @@ goal_1 = 54
 Using the +-operator, create a string that reports on who scored when, according to the format:
 <scorer_name> <when_they_scored>, <scorer_name> <when_they_scored>
 """
-scorers = f"{Goal_scorer_0} {goal_0}, {Goal_scorer_1} {goal_1}"
-print(scorers)
+scorers = f"{goal_scorer_0} {goal_0}, {goal_scorer_1} {goal_1}"
+scorers_plus = goal_scorer_0 + " " + str(goal_0) + ", " + goal_scorer_1 + " " + str(goal_1)
+print(scorers_plus)
 
 # Use f-strings or the +-operator to create a single string with information about who scored when in the format:
 
@@ -24,30 +28,36 @@ print(scorers)
 # <scorer_name> scored in the <when_they_scored>th minute
 # The result should be stored in a variable report.
 
-report = f'{Goal_scorer_0} scored in the {goal_0}nd minute\n{Goal_scorer_1} scored in the {goal_1}th minute'
+report = f'{goal_scorer_0} scored in the {goal_0}nd minute\n{goal_scorer_1} scored in the {goal_1}th minute'
 print(report)
 
 # Store the following values in the given variable, and do each of those exercises in a single line of code. Make sure that your code still produces the correct results for different values for the player  value.
 # 1. Choose a player that played in the soccer match and store his name as a string in the variable player.
-player = Goal_scorer_1
+player = "David van den Berg"
 
 # 2.first_name: use slicing and the find-method (help) to isolate and store the player's first name.
-first_name = player[0:5]
+first_name_x = player.find(" ")
+first_name = player[0:first_name_x]
 
 # 3. last_name_len: use find, slicing and len to isolate and store the length of their last name.
-last_name = player[6:]
+last_name_x = first_name_x + 1
+last_name = player[last_name_x:]
 last_name_len = len(last_name)
 
 # 4. name_short: isolate and store the player's name in this format:
 # G. von Examplestein
-name_short = "M. van Basten"
+name_short = player[0:1] + ". " + last_name
+print(name_short)
 
 # 5. chant: this is what the crowd chants when it looks like your player is going to score a goal -- their first name plus an exclamation mark(!), x-times, where x is the number of characters in their first name. 
 # Make sure the last character of this string is not a space! For our example player:
 # Gut! Gut! Gut!
-chant = "Marco! Marco! Marco! Marco! Marco!"
+amount_chant = len(first_name)
+chant_pre = ((first_name + "! ") * amount_chant) 
+chant = chant_pre[:-1]
+print(chant)
 
 # 6. good_chant: Make super-sure the last character of chant is not a space by using the inequality operator (!=). Try this in your REPL for an example: print(2 != 3). Also try: print(2 != 2).
-good_chant =(chant[-1] != '')
-
+good_chant =(chant[-1] != " ")
+print(good_chant)
 
